ddba4a2f32e0f07c728d4ceab39be9e776012006
Hans Hohenfeld <hans.hohenfeld@dfki.de>
2022-07-31 10:59:25 +0200

    C++ settings

---
+++ ftplugin/cpp.lua
@@ -0,0 +1,3 @@
+vim.bo.tabstop = 2        -- insert 4 spaces for a tab
+vim.bo.shiftwidth = 2     -- the number of spaces inserted for each indentation
+vim.bo.softtabstop = 2        -- Number of spaces for a tab while editing
