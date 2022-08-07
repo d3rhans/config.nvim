fe46e4e88e0f799f1416fb32b0de951dbc4b40ec
Hans Hohenfeld <hans.hohenfeld@posteo.de>
2022-08-07 14:56:13 +0200

    Formatting

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,7 +4,7 @@ vim.opt.textwidth = 99
 
 local dap_python_loaded, dap_python = pcall(require, "dap-python")
 if dap_python_loaded then
-	dap_python.setup('~/Code/venvs/debugpy/bin/python')
+    dap_python.setup('~/Code/venvs/debugpy/bin/python')
 
     local silent_opt = { silent = true }
     km.nnoremap("<leader>dm", dap_python.test_method, silent_opt)
