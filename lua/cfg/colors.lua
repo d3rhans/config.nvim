d26b58d7d05c869537ec55578196e5439e21420e
Hans Hohenfeld <hans.hohenfeld@posteo.de>
2022-11-23 03:02:52 +0100

    Updated color

--- lua/cfg/colors.lua
+++ lua/cfg/colors.lua
@@ -1,1 +1,23 @@
-vim.cmd[[colorscheme catppuccin]]
+local status_ok, nf = pcall(require, "nightfox")
+if not status_ok then
+    return
+end
+
+nf.setup({
+    options = {
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+            comments = "italic",
+            keywords = "bold",
+        },
+    },
+
+    groups = {
+        terafox = {
+            CursorLine = { bg = "bg2" },
+        }
+    },
+})
+
+vim.cmd("colorscheme terafox")
